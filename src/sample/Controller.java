18ed1c9a4c0a87b790da92d307988ce8ec46b738
CHABRIER Valentin <chabrier.valentin@gmail.com>
2018-03-29 16:32:41 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+import manageFiles.ParseFiles;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Controller {
+
+/*   ParseFiles parser = new ParseFiles("src/data/data01.txt");
+        try {
+        ArrayList<Client> clients =  parser.createClientsFromFile();
+        for (Client client: clients) {
+            System.out.println(client.getId());
+        }
+    }
+
+
+
+        catch (FileNotFoundException e){
+        e.printStackTrace();
+        System.out.println("file doesn't exist");
+    }
+        catch (IOException e) {
+        e.printStackTrace();
+
+    }*/
+}
